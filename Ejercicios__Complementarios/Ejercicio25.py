ac0036e78b532bff6dd2f890717477d1f5b6a4eb
Felipev98 <felipe_v_spd@hotmail.com>
2021-05-18 12:38:50 -0400

    Ejercicios

---
+++ Ejercicios__Complementarios/Ejercicio25.py
@@ -0,0 +1,8 @@
+#Dado un número, cuente el número total de dígitos de un número
+#Por ejemplo, el número es 75869, por lo que la salida debería ser 5. Utilizando for
+
+numero = input('Escriba un número: ')
+for i in numero:
+    contar = len(numero)
+print('La cantidad de carácteres de su número es de: ',contar)
+
